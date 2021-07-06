d458c474f1d9491674f31b786372244ca72812a2
起点 <coolspy@qq.com>
2021-07-06 19:57:56 +0800

    refactor: 初始化项目结构

---
+++ core/cli/bin/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const utils = require('utils')
+
+
+console.log('hello magical-cli')
+utils()
---
+++ utils/utils/__tests__/utils.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const utils = require('../lib/utils');
+
+describe('utils', () => {
+    it('needs tests');
+});
